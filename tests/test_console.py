584499508f4d698b05fa3460db429ae6fda0c1fc
Artemii <40743076+artemii-yanushevskyi@users.noreply.github.com>
2019-05-31 23:07:58 +0300

    Change the dir to an existing one

--- tests/test_console.py
+++ tests/test_console.py
@@ -4,7 +4,7 @@ import os
 
 class TestCosoleInput(unittest.TestCase):
     def test_path(self):
-        output = subprocess.run(['./SimilarPictureFinder.py', '--path', 'a_path/to/folder_with_images'], stdout=subprocess.PIPE)
+        output = subprocess.run(['./SimilarPictureFinder.py', '--path', 'tests/test_same'], stdout=subprocess.PIPE)
         output_text = output.stdout.decode('utf-8')
         output_first_line = output_text.split('\n')[0]
         project_folder_path = os.getcwd()
